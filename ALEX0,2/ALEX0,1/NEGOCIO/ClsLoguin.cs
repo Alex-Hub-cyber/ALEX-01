ad015a09444aead525ecf96c9875d4876750939f
Alex-Hub-cyber <gl0430032020@unab.edu.sv>
2021-03-06 13:57:54 -0600

    PARTE 2
    PROYECTO PARTE 2

---
+++ ALEX0,2/ALEX0,1/NEGOCIO/ClsLoguin.cs
@@ -0,0 +1,22 @@
+﻿using ALEX0_1.DOMINIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALEX0_1.NEGOCIO
+{
+    class ClsLoguin
+    {
+    
+        public int acceso(Login log)
+        {
+            int estado = 0;
+            if (log.User.Equals("Alex")&& log.Password.Equals("123")) {
+                estado = 1;
+            }
+            return estado ;
+        }
+    }
+}
---
+++ HISTORIALPROYECTO/ALEX0,2/ALEX0,1/VISTA/FrmMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ALEX0_1.VISTA
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cLICKAQUIPARAMASINFORMACIÓNToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmAcercade frm = new FrmAcercade();
+            frm.MdiParent = this;
+            frm.Show();
+        }   
+    }
+}
